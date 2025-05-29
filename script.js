204dae84a8b5855fdcd719120ec180a263af11ab
dotlaboratory <night.fishing.2233@gmail.com>
2025-05-30 07:00:36 +0900

    寝ようよ…

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function onClickChara(id){
+  	fetch(`assets/${id}.html`) //ロード元URL
+	.then(data => data.text())
+	.then(html => document.getElementById("content").innerHTML = html);
     Fancybox.show([
   {
-    src: `assets/${id}.html`,
-    type: "iframe",
+    src: `#content`,
+    type: "inline",
   },
 ]);
+//     Fancybox.show([
+//   {
+//     src: `assets/${id}.html`,
+//     type: "iframe",
+//   },
+// ]);
 }
