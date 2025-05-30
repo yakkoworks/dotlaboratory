e29212eef432e6712260acaea99f773087aef600
dotlaboratory <night.fishing.2233@gmail.com>
2025-05-31 06:55:05 +0900

    みための調整

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 async function init(){
+	setZoomScale();
 	await loadProfile("1","1-1");
 	await loadProfile("1","1-4k");
 	carousel_team1 = new Carousel(document.getElementById("carousel-team1"), {
@@ -33,4 +34,24 @@ async function loadProfile(team,chara){
 			resolve('OK');
 		})
 	});
+}
+
+function setZoomScale(){
+	var dummy = document.createElement("div");
+	dummy.innerText = "テスト";
+	dummy.id = "sizing";
+	dummy.style.fontSize = "16px";
+	dummy.style.width = "1px"
+	dummy.style.height = "1px"
+	dummy.style.visibility = "hidden";
+	document.body.append(dummy);
+	var fontSize = $("#sizing").css("font-size");
+	var diameter = $("#sizing").css("width");
+	dummy.remove();
+	var reg = /(.+)px/;
+	fontSize = 16 * 16 / reg.exec(fontSize)[1];
+	diameter =  reg.exec(diameter)[1];
+	document.body.style.setProperty('--diameter', `${diameter}`);
+	document.body.style.setProperty('--rootFontSize', `${fontSize}`);
+	document.querySelector("html").style.fontSize = `${fontSize}px`;
 }
