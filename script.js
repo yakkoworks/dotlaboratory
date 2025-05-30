7fd1ff10fc83b8033b3c549666efb8b3c198cc79
dotlaboratory <night.fishing.2233@gmail.com>
2025-05-30 20:20:35 +0900

    プロフィールの読み込み

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function onClickChara(id){
-  	fetch(`assets/${id}.html`) //ロード元URL
-	.then(data => data.text())
-	.then(html => document.getElementById("content").innerHTML = html);
-    Fancybox.show([
-  {
-    src: `#content`,
-    type: "inline",
-  },
-]);
+async function init(){
+	await loadProfile("1","1-1");
+	await loadProfile("1","1-4k")
+}
+
+async function loadProfile(team,chara){
+	return new Promise((resolve) => {
+		fetch(`assets/${chara}.html`)
+		.then(data => data.text())
+		.then(html => {
+			const parser = new DOMParser();
+			var dom = parser.parseFromString(html, "text/html");
+			document.querySelector(`#profiles #team${team}`).appendChild(dom.documentElement.querySelector("body>div"));
+			resolve('OK');
+		})
+	});
 }
