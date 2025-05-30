ee59631eb01338c43ceb054a17c4802dd6657274
dotlaboratory <night.fishing.2233@gmail.com>
2025-05-31 00:07:15 +0900

    こまめにコミットしようや

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 async function init(){
 	await loadProfile("1","1-1");
-	await loadProfile("1","1-4k")
+	await loadProfile("1","1-4k");
+	carousel_team1 = new Carousel(document.getElementById("carousel-team1"), { infinite: false,preload:0,Dots:false});
+	nav_team1 = new Carousel(document.querySelector("#nav-team1"),{
+		infinite: false,
+        transition: false,
+        // center: true,
+        // fill: true,
+        slidesPerPage: 1,
+        dragFree: true,
+        Dots: false,
+		Sync: {target: carousel_team1}
+	});
 }
 
 async function loadProfile(team,chara){
 	return new Promise((resolve) => {
-		fetch(`assets/${chara}.html`)
+		fetch(`assets/slides/${chara}.html`)
 		.then(data => data.text())
 		.then(html => {
 			const parser = new DOMParser();
 			var dom = parser.parseFromString(html, "text/html");
-			document.querySelector(`#profiles #team${team}`).appendChild(dom.documentElement.querySelector("body>div"));
+			document.querySelector(`#carousel-team${team}`).appendChild(dom.documentElement.querySelector("body>div"));
+			console.log("loadProfile");
 			resolve('OK');
 		})
 	});
 }
+function onClickChara(team,chara){
+	Fancybox.show(
+		[{src:`#${chara}`,}],
+		{closeButton:true,autoFocus:false})
+	}
